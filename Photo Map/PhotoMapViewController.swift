a42512fe36806451dbd553d70cac67708036df5a
John Lawrence Henning <jlhbaseball15@me.com>
2016-03-10 18:55:28 -0500

    added map view and button

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import MapKit
 
 class PhotoMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var captureButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //makes UIButton circular
+//        captureButton.imageView?.layer.cornerRadius = ((captureButton.imageView?.bounds.size.height)!/2);
+        
+        
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+            MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
+        
         // Do any additional setup after loading the view.
     }
 
